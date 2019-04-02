f21071e6dfc69be201197ac8c4bbed37d5c1f9b7
Carlos <1310374@utp.edu.pe>
2019-04-02 14:19:16 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,18 +8,33 @@ import { InfoPagina } from "../interfaces/info-pagina.interface";
 export class InfoPaginaService {
   info: InfoPagina;
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
     //console.log("Servicio de infoPagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http
       .get("assets/data/data-pagina.json")
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp["email"]);
-        console.log(resp.instagram);
+        //console.log(resp["email"]);
+        //console.log(resp.instagram);
+      });
+  }
+
+  private cargarEquipo() {
+    // Leer servicio de Firebase
+    this.http
+      .get("https://angular-html-f6e65.firebaseio.com/equipo.json")
+      .subscribe( (resp: any) => {
+        this.equipo = resp;
+        //console.log(resp);
       });
   }
 }
